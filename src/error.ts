9293e3f5876b89c150478f41e5f401b99e01592e
Tilman Roeder <dyed.green.info@gmail.com>
2020-03-03 16:40:12 +0000

    Correct status names in documentation.

--- src/error.ts
+++ src/error.ts
@@ -15,7 +15,7 @@ export default class SqliteError extends Error {
   constructor(message: string, code?: number) {
     super(message);
     this.name = "SqliteError";
-    this.code = code ?? undefined;
+    this.code = code ?? Status.Unknown;
   }
 
   /**
@@ -31,31 +31,36 @@ export default class SqliteError extends Error {
    * can also contain custom status codes specific
    * to this library (starting from 1000).
    *
+   * Errors that originate in the JavaScript part of
+   * the library will not have an associated status
+   * code. For these errors, the code will be
+   * `Status.Unknown`.
+   *
    * | JS name          | code | JS name (cont.)  | code |
    * |------------------|------|------------------|------|
-   * | sqliteOk         | 0    | sqliteTooBig     | 18   |
-   * | sqliteError      | 1    | sqliteConstraint | 19   |
-   * | sqliteInternal   | 2    | sqliteMismatch   | 20   |
-   * | sqlitePerm       | 3    | sqliteMisuse     | 21   |
-   * | sqliteAbort      | 4    | sqliteNoLFS      | 22   |
-   * | sqliteBusy       | 5    | sqliteAuth       | 23   |
-   * | sqliteLocked     | 6    | sqlietFormat     | 24   |
-   * | sqliteNoMem      | 7    | sqliteRange      | 25   |
-   * | sqliteReadOnly   | 8    | sqliteNotADB     | 26   |
-   * | sqliteInterrupt  | 9    | sqliteNotice     | 27   |
-   * | sqliteIOErr      | 10   | sqliteWarning    | 28   |
-   * | sqliteCorrupt    | 11   | sqliteRow        | 100  |
-   * | sqliteNotFound   | 12   | sqliteDone       | 101  |
-   * | sqliteFull       | 13   | stmtLimit        | 1000 |
-   * | sqliteCantOpen   | 14   | noStmt           | 1001 |
-   * | sqliteProtocol   | 15   | databaseLimit    | 1002 |
-   * | sqliteEmpty      | 16   | noDatabase       | 1003 |
-   * | sqliteSchema     | 17   |                  |      |
+   * | SqliteOk         | 0    | SqliteTooBig     | 18   |
+   * | SqliteError      | 1    | SqliteConstraint | 19   |
+   * | SqliteInternal   | 2    | SqliteMismatch   | 20   |
+   * | SqlitePerm       | 3    | SqliteMisuse     | 21   |
+   * | SqliteAbort      | 4    | SqliteNoLFS      | 22   |
+   * | SqliteBusy       | 5    | SqliteAuth       | 23   |
+   * | SqliteLocked     | 6    | sqlietFormat     | 24   |
+   * | SqliteNoMem      | 7    | SqliteRange      | 25   |
+   * | SqliteReadOnly   | 8    | SqliteNotADB     | 26   |
+   * | SqliteInterrupt  | 9    | SqliteNotice     | 27   |
+   * | SqliteIOErr      | 10   | SqliteWarning    | 28   |
+   * | SqliteCorrupt    | 11   | SqliteRow        | 100  |
+   * | SqliteNotFound   | 12   | SqliteDone       | 101  |
+   * | SqliteFull       | 13   | StmtLimit        | 1000 |
+   * | SqliteCantOpen   | 14   | NoStmt           | 1001 |
+   * | SqliteProtocol   | 15   | DatabaseLimit    | 1002 |
+   * | SqliteEmpty      | 16   | NoDatabase       | 1003 |
+   * | SqliteSchema     | 17   | Unknown          | -1   |
    *
    * These codes are accessible via
    * the exported `Status` object.
    */
-  code?: number;
+  code: number;
 
   /**
    * SqliteError.codeName
@@ -67,8 +72,6 @@ export default class SqliteError extends Error {
    * `codeName` would be `SqliteConstraint`.
    */
   get codeName(): keyof typeof Status {
-    return (this.code
-      ? Status[this.code]
-      : Status[Status.Unknown]) as keyof typeof Status;
+    return Status[this.code] as keyof typeof Status;
   }
 }
