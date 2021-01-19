1bafc82462569020c70fc9343fc114d35df924c7
Jeremy Banks <_@jeremy.ca>
2021-01-19 11:26:29 -0500

    minor tweaks re: review

--- build/hack/bundle.js
+++ build/hack/bundle.js
@@ -5,10 +5,10 @@ const wasm = await Deno.readFile(src);
 function encode(bytes) {
   let binary = "";
   let len = bytes.length;
-  for (let i = 0; i < len; i++) {
+  for (var i = 0; i < len; i++) {
     binary += String.fromCharCode(bytes[i]);
   }
-  return window.btoa(binary).replace(/.{72}/g, "$&\n");
+  return window.btoa(binary).replace(/\n/g, "");
 }
 
 await Deno.writeFile(
@@ -16,9 +16,8 @@ await Deno.writeFile(
   new TextEncoder().encode(
     `import env from "./vfs.js";
 
-const wasm = \`
-${encode(wasm)}
-\`;
+const wasm =
+  "${encode(wasm)}";
 
 function decode(base64) {
   const bytesStr = atob(base64);
