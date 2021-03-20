e1d625fb61b8283eafe20da0842cfff237aab2a3
Tilman Roeder <dyed.green.info@gmail.com>
2021-03-20 12:57:00 +0000

    Add type generation for WASM bindings

---
+++ build/hack/gen_types.ts
@@ -0,0 +1,146 @@
+interface Item {
+  name: string;
+  arguments: Argument[];
+  returnType: Type;
+}
+
+interface Argument {
+  name: string;
+  type: Type;
+}
+
+enum Type {
+  Void,
+  VoidPtr,
+  StringPtr,
+  StatementPtr,
+  Double,
+  Int,
+}
+
+const items = [
+  // exported manually in compiler invocation
+  {
+    name: "malloc",
+    arguments: [{ name: "size", type: Type.Int }],
+    returnType: Type.VoidPtr,
+  },
+  {
+    name: "free",
+    arguments: [{ name: "ptr", type: Type.VoidPtr }],
+    returnType: Type.Void,
+  },
+];
+
+const [src, dest] = Deno.args;
+const wrapperSrc = await Deno.readTextFile(src);
+
+// int EXPORT(bind_int) (sqlite3_stmt* stmt, int idx, double value)
+const typeRegexp =
+  `(const +)?(void|sqlite3_stmt\\*|char\\*|void\\*|int|double)`;
+const argRegexp = `${typeRegexp} +[a-z_]+`;
+const exportSignature = new RegExp(
+  `${typeRegexp} +EXPORT\\([a-z_]+\\) +\\(${argRegexp}( *, *${argRegexp})*\\)`,
+);
+
+function nullThrows<T>(value: T | null | undefined): T {
+  if (value == null) {
+    throw new Error("Got a null value");
+  }
+  return value as T;
+}
+
+function typeFromCType(cType: string): Type {
+  cType = cType.replace("const", "").replace(/ /g, "");
+  switch (cType) {
+    case "void":
+      return Type.Void;
+    case "void*":
+      return Type.VoidPtr;
+    case "char*":
+      return Type.StringPtr;
+    case "sqlite3_stmt*":
+      return Type.StatementPtr;
+    case "double":
+      return Type.Double;
+    case "int":
+      return Type.Int;
+    default:
+      throw new Error("Unknown type");
+  }
+}
+
+function getReturnType(line: string): Type {
+  const regexp = new RegExp(typeRegexp);
+  const [, _const, cType] = nullThrows(regexp.exec(line));
+  return typeFromCType(cType);
+}
+
+function getName(line: string): string {
+  const [, name] = nullThrows(/EXPORT\(([a-z_]+)\)/.exec(line));
+  return name;
+}
+
+function getArguments(line: string): Argument[] {
+  const [, argList] = nullThrows(/EXPORT\([a-z_]+\) *\(([^)]+)\)/.exec(line));
+  return argList.split(",").map((arg) => {
+    const regexp = new RegExp(`${typeRegexp} +([a-z_]+)`);
+    const [, _const, cType, name] = nullThrows(regexp.exec(arg));
+    return {
+      name,
+      type: typeFromCType(cType),
+    };
+  });
+}
+
+function generateType(tp: Type): string {
+  switch (tp) {
+    case Type.Void:
+      return "void";
+    case Type.VoidPtr:
+      return "VoidPtr";
+    case Type.StringPtr:
+      return "StringPtr";
+    case Type.StatementPtr:
+      return "StatementPtr";
+    case Type.Int:
+    case Type.Double:
+      return "number";
+    default:
+      throw new Error("Unknown type");
+  }
+}
+
+function generateDecl(item: Item): string {
+  const args = item.arguments.map((arg) =>
+    `${arg.name}: ${generateType(arg.type)}`
+  ).join(", ");
+  return `${item.name}: (${args}) => ${generateType(item.returnType)}`;
+}
+
+const exportLines = wrapperSrc.split("\n").filter((line) =>
+  exportSignature.test(line)
+);
+
+for (const line of exportLines) {
+  const name = getName(line);
+  const returnType = getReturnType(line);
+  const args = getArguments(line);
+
+  items.push({ name, returnType, arguments: args });
+}
+
+const typeDeclaration = `// Deno SQLite WASM binding types
+
+type VoidPtr = number;
+type StringPtr = number;
+type StatementPtr = number;
+
+export interface Wasm {
+  ${items.map(generateDecl).join(";\n  ")};
+}
+
+export default function instantiate(): { exports: Wasm };
+`;
+
+await Deno.writeTextFile(dest, typeDeclaration);
---
+++ build/sqlite.d.ts
@@ -0,0 +1,40 @@
+// Deno SQLite WASM binding types
+
+type VoidPtr = number;
+type StringPtr = number;
+type StatementPtr = number;
+
+export interface Wasm {
+  malloc: (size: number) => VoidPtr;
+  free: (ptr: VoidPtr) => void;
+  str_len: (str: StringPtr) => number;
+  seed_rng: (seed: number) => void;
+  open: (filename: StringPtr) => number;
+  prepare: (sql: StringPtr) => StatementPtr;
+  finalize: (stmt: StatementPtr) => number;
+  bind_int: (stmt: StatementPtr, idx: number, value: number) => number;
+  bind_double: (stmt: StatementPtr, idx: number, value: number) => number;
+  bind_text: (stmt: StatementPtr, idx: number, value: StringPtr) => number;
+  bind_blob: (
+    stmt: StatementPtr,
+    idx: number,
+    value: VoidPtr,
+    size: number,
+  ) => number;
+  bind_big_int: (stmt: StatementPtr, idx: number, value: StringPtr) => number;
+  bind_null: (stmt: StatementPtr, idx: number) => number;
+  bind_parameter_index: (stmt: StatementPtr, name: StringPtr) => number;
+  step: (stmt: StatementPtr) => number;
+  column_count: (stmt: StatementPtr) => number;
+  column_type: (stmt: StatementPtr, col: number) => number;
+  column_int: (stmt: StatementPtr, col: number) => number;
+  column_double: (stmt: StatementPtr, col: number) => number;
+  column_text: (stmt: StatementPtr, col: number) => StringPtr;
+  column_blob: (stmt: StatementPtr, col: number) => void;
+  column_bytes: (stmt: StatementPtr, col: number) => number;
+  column_name: (stmt: StatementPtr, col: number) => StringPtr;
+  column_origin_name: (stmt: StatementPtr, col: number) => StringPtr;
+  column_table_name: (stmt: StatementPtr, col: number) => StringPtr;
+}
+
+export default function instantiate(): { exports: Wasm };
