3a56e4af1b11adbb313a9a73c95d4d210c3efdb0
Tilman Roeder <dyed.green.info@gmail.com>
2021-05-12 13:18:06 +0100

    Add import for Rows in mod.ts for deno doc
    Deno doc can not discover the rows type by itself, since it does not
    parse files recursively. This means that we need to add the Rows import
    manually for deno doc to be able to link the exposed Rows type in the
    DB class etc...

--- mod.ts
+++ mod.ts
@@ -4,3 +4,6 @@ export { Status } from "./src/constants.ts";
 
 export type { PreparedQuery, QueryParam } from "./src/db.ts";
 export type { ColumnName } from "./src/rows.ts";
+
+// imports for deno doc
+import { Rows } from "./src/rows.ts";
