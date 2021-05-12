e03b97bddfd1a6783147e721482b52f77948a13f
Tilman Roeder <dyed.green.info@gmail.com>
2021-05-12 11:26:24 +0100

    Export interface definitions

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
 export { DB } from "./src/db.ts";
 export { Empty } from "./src/rows.ts";
 export { Status } from "./src/constants.ts";
+
+export type { PreparedQuery, QueryParam } from "./src/db.ts";
+export type { ColumnName } from "./src/rows.ts";
