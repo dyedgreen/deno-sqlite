ff1a1e6f4484b67b869a942c3eccb9da47b54acd
Tilman Roeder <dyed.green.info@gmail.com>
2022-01-01 23:58:30 +0000

    Expose writing to files

--- browser/mod.ts
+++ browser/mod.ts
@@ -1,5 +1,5 @@
 import { DB } from "../src/db.ts";
-import { loadFile } from "./vfs.js";
+import { loadFile, writeFile } from "./vfs.js";
 
 export { SqliteError } from "../src/error.ts";
 export { Status } from "../src/constants.ts";
@@ -7,6 +7,10 @@ export { Status } from "../src/constants.ts";
 export async function open(file: string): Promise<DB> {
   await loadFile(file);
   return new DB(file);
+}
+
+export async function write(file: string, data: Uint8Array): Promise<void> {
+  await writeFile(file, data);
 }
 
 export async function read(file: string): Promise<Uint8Array> {
