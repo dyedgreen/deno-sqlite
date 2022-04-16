28619024e557a9bd04e9c3e5d9173fa89416e397
Jesse Jackson <jsejcksn@users.noreply.github.com>
2022-04-16 18:44:07 -0500

    build: update dependencies
    - std@0.135.0

--- dev_deps.ts
+++ dev_deps.ts
@@ -1,11 +1,11 @@
 export {
   bench,
   runBenchmarks,
-} from "https://deno.land/std@0.134.0/testing/bench.ts";
+} from "https://deno.land/std@0.135.0/testing/bench.ts";
 export {
   assertAlmostEquals,
   assertEquals,
   assertInstanceOf,
   assertMatch,
   assertThrows,
-} from "https://deno.land/std@0.134.0/testing/asserts.ts";
+} from "https://deno.land/std@0.135.0/testing/asserts.ts";
